16771f54c9934756e7edf54316e258e607f760fc
MiguelDonado <miguel.5799@hotmail.es>
2024-10-29 13:00:06 +0100

    Finish helper script python project

---
+++ utils_dev_purposes_scrapingFincas.sh
@@ -0,0 +1,47 @@
+# This is script has been created to facilitate developing and debugging of the scrapingFincasHacienda project. 
+# Mainly to clean the logs folder, and each subfolder inside data folder as well as to remove the FincasProject.db.
+
+DATAPATH="/home/miguel/coding-projects/ScrapingFincasHacienda/data"
+LOGSPATH="/home/miguel/coding-projects/ScrapingFincasHacienda/scrapingFincasHacienda/logs"
+DBPATH="/home/miguel/coding-projects/ScrapingFincasHacienda/scrapingFincasHacienda/FincasProject.db"
+
+folder_remove(){
+     # Check log folder exists
+     if [ -d $1 ];then
+          # Check if the directory is empty
+          if [ -z "$(ls -A "$1")" ];then
+               echo "The directory $1 is empty."
+          else
+               # Rm each file inside logs folder
+               for file in $(ls $1);do
+                    rm "$1/$file"
+                    echo "The file $1/$file has been removed."
+               done
+          fi
+     else
+          echo "The directory $1 does not exist".
+}
+
+# If database file exists, then remove it.
+if [ -f $DBPATH ];then
+     rm "$DBPATH"
+     echo "The database $DBPATH has been removed"
+fi
+
+# Empty logs folder
+folder_remove $LOGSPATH
+
+# List subfolders in '/data' folder
+subfolders=$(ls "$DATAPATH")
+
+# Empty each '/data/subfolder' inside '/data' folder
+for subfolder in $subfolders;do
+     # Get full path of the subfolders
+     fullpath_subfolder="$DATAPATH/$subfolder"
+
+     # Empty subfolder
+     folder_remove $fullpath_subfolder
+done
+    
+
+
